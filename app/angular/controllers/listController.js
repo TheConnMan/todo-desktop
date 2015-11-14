c2ff53d2ee78705a2388970e116bf325ff84db9e
TheConnMan <bcconn2112@gmail.com>
2015-11-14 12:56:12 -0500

    Add Item creation

--- app/angular/controllers/listController.js
+++ app/angular/controllers/listController.js
@@ -4,5 +4,26 @@
 	var app = angular.module('app');
 	app.controller('listController', ['$scope', function($scope) {
 		$scope.header = 'ToDo Header';
+		$scope.adding = false;
+		$scope.newItem = '';
+
+		$scope.items = [];
+
+		$scope.createItem = function() {
+			$scope.adding = true;
+			$scope.newItem = '';
+			setTimeout(function() {
+				$('#itemName').focus();
+			}, 0);
+		};
+
+		$scope.submitItem = function() {
+			var item = {
+				name: $scope.newItem,
+				dateCreated: new Date()
+			};
+			$scope.items.push(item);
+			$scope.adding = false;
+		};
 	}]);
 })();
