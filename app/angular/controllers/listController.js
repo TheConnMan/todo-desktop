60ddd66412175b4d572e22bc7d1290100c88cd77
TheConnMan <bcconn2112@gmail.com>
2015-11-14 14:03:14 -0500

    Add persistance to localstorage

--- app/angular/controllers/listController.js
+++ app/angular/controllers/listController.js
@@ -7,8 +7,6 @@
 		$scope.adding = false;
 		$scope.newItem = '';
 
-		$scope.items = [];
-
 		$scope.createItem = function() {
 			$scope.adding = true;
 			$scope.newItem = '';
@@ -20,14 +18,30 @@
 		$scope.submitItem = function() {
 			var item = {
 				name: $scope.newItem,
-				dateCreated: new Date(),
+				dateCreated: new Date().toISOString(),
 				complete: false
 			};
 			$scope.items.push(item);
 			$scope.adding = false;
+			$scope.saveItems();
 			setTimeout(function() {
 				$('.timeago').timeago();
 			}, 0);
 		};
+
+		$scope.saveItems = function() {
+			localStorage.setItem('todo-items', JSON.stringify($scope.items));
+		};
+
+		$scope.loadItems = function() {
+			var itemString = localStorage.getItem('todo-items');
+			return itemString ? JSON.parse(itemString) : [];
+		};
+
+		$scope.items = $scope.loadItems();
+
+		setTimeout(function() {
+			$('.timeago').timeago();
+		}, 0);
 	}]);
 })();
